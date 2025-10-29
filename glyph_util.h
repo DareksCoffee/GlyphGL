589220b63fc3ebe089a271dcd728d1910d7f984e
Darek <72472681+DareksCoffee@users.noreply.github.com>
2025-10-29 08:26:00 +0000

    Glyph version V0.0.3
    -  Added vertex batching
    - Implemented UTF-8 support
    - Implemented custom memory allocation macros (for now they're basic)
    - added GLYPHGL_CHARSET_BASIC, GLYPHGL_CHARSET_DEFAULT macros for easier atlas generation

---
+++ glyph_util.h
@@ -0,0 +1,14 @@
+#ifndef GLYPH_UTIL_H
+#define GLYPH_UTIL_H
+
+#include <stdlib.h>
+#ifndef GLYPH_MALLOC
+#define GLYPH_MALLOC malloc
+#endif
+
+#ifndef GLYPH_FREE
+#define GLYPH_FREE free
+#endif
+
+
+#endif
